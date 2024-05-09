1d46eb9f16c5b483f7fd80a7f2d3e95e7e273a54
sunwoo <114386406+Kimxxunu@users.noreply.github.com>
2024-05-10 04:28:41 +0900

    Feat: Spring 프로젝트 생성, Related to: #1

---
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/HallymFestival2024BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hallymfestival.HallymFestival2024BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HallymFestival2024BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HallymFestival2024BackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hallymfestival/HallymFestival2024BackEnd/HallymFestival2024BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hallymfestival.HallymFestival2024BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HallymFestival2024BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
