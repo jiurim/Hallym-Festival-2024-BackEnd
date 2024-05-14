3b97bf9c44b7f76b5f61fadcafd373ee1c7b7ba5
urim <jiurim0316@naver.com>
2024-05-15 02:17:18 +0900

    Fix : Security 기본 화면 제거 기능 수정

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/HallymFestival2024BackEndApplication.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/HallymFestival2024BackEndApplication.java
@@ -2,8 +2,9 @@ package com.hallymfestival.HallymFestival2024BackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class HallymFestival2024BackEndApplication {
 
 	public static void main(String[] args) {
