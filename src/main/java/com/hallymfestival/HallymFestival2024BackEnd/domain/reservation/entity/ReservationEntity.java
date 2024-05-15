7064a594106983bad60a80cd1dee4541d66b3620
urim <jiurim0316@naver.com>
2024-05-16 06:15:54 +0900

    Fix : entity id 자동 할당 수정

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/domain/reservation/entity/ReservationEntity.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/domain/reservation/entity/ReservationEntity.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @Table(name="reservation")
 public class ReservationEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
     private String studentId;
