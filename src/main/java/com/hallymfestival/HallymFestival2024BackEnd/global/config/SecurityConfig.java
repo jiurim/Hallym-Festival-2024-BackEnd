8d0b8da6884d962d95db32c1bcf2d960db18e19a
urim <jiurim0316@naver.com>
2024-05-17 06:03:30 +0900

    Feat : 관리자 도메인 CORS 설정 해제

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/SecurityConfig.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 //HttpServletRequest를 사용하는 요청들에 대한 접근 제한 설정.
-                //.antMatchers("/api/admin/community", "/api/admin/notice", "/api/admin/find").hasRole("ADMIN")
+                .antMatchers("/api/admin/community", "/api/admin/notice", "/api/admin/find").hasRole("ADMIN")
                 //위 주소는 관리자만 접근
                 .antMatchers("/**").permitAll()
                 .antMatchers("http://13.209.218.51/api/env").permitAll()
