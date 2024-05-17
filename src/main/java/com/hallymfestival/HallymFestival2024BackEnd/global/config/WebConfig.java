e90dd9b1dbf42e2c67ac7ab43e7b996a8b152e98
urim <jiurim0316@naver.com>
2024-05-17 12:10:47 +0900

    Fix : Cors 해결 위한 WebConfig 수정2

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/WebConfig.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/WebConfig.java
@@ -12,11 +12,11 @@
         public void addCorsMappings(CorsRegistry registry) {
 
             registry.addMapping("/**")
-                    .allowedOrigins("http://hallym-festival.com","https://hallym-festival.com","http://github-action-s3-hallym-festival-develop.s3-website.ap-northeast-2.amazonaws.com","http://github-action-s3-list-app.s3-website.ap-northeast-2.amazonaws.com","http://localhost:8080", "http://localhost:5175","http://localhost:5173","http://localhost:8081", "http://localhost:3000","http://3.39.62.170","http://13.209.218.51") // 허용할 출처 추가
+                    .allowedOrigins("https://hallym-festival-admin.com","http://hallym-festival-admin.com","http://hallym-festival.com","https://hallym-festival.com","http://github-action-s3-hallym-festival-develop.s3-website.ap-northeast-2.amazonaws.com","http://github-action-s3-list-app.s3-website.ap-northeast-2.amazonaws.com","http://localhost:8080", "http://localhost:5175","http://localhost:5173","http://localhost:8081", "http://localhost:3000","http://3.39.62.170","http://13.209.218.51") // 허용할 출처 추가
                     .allowedMethods("GET", "POST","DELETE","PUT") // 허용할 HTTP method
                     .allowCredentials(true) // 쿠키 인증 요청 허용
                     //.allowedOriginPatterns("*") //추가한 부분
-                    .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱2g
+                    .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱2
         }
 //
 
