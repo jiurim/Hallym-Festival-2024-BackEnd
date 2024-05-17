10ae5a55e018ce1be2ac2c406c12db4a620aa298
urim <jiurim0316@naver.com>
2024-05-18 07:31:45 +0900

    Fix : Security Config 수정

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/domain/manager/config/SecurityConfig.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/domain/manager/config/SecurityConfig.java
@@ -50,8 +50,8 @@ public class SecurityConfig{
                 .antMatchers("/api/admin/community/create/","/api/admin/community/{id}", "/api/admin/notice/create/","/api/admin/notice/{id}", "/api/admin/find/create/", "/api/admin/find/{id}").hasRole("ADMIN")
                 .antMatchers("/api/env", "/api/hc").permitAll()
                 .antMatchers("/api/admin/login").permitAll()
+                .antMatchers("https://hallym-festival-admin.com/").permitAll()
                 .antMatchers("/**").permitAll()
-                .antMatchers("https://hallym-festival-admin.com/")
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
                 .and()
