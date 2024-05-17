19bb1ed519b6815fb7942fafda2d9a0adef9934c
urim <jiurim0316@naver.com>
2024-05-18 07:36:14 +0900

    Fix : 중복된 Cors url 제거

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/WebConfig.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://hallym-festival-admin.com","http://hallym-festival-admin.com","http://hallym-festival.com", "https://hallym-festival.com", "http://github-action-s3-hallym-festival-develop.s3-website.ap-northeast-2.amazonaws.com", "http://github-action-s3-list-app.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:8080", "http://localhost:5175", "http://localhost:5173", "http://localhost:8081", "http://localhost:3000", "http://3.39.62.170", "http://13.209.218.51","http://hallym-festival-admin.com","https://hallym-festival-admin.com","https://kim-sun-woo.com/") // 허용할 출처 추가
+                .allowedOrigins("https://hallym-festival-admin.com","http://hallym-festival-admin.com","http://hallym-festival.com", "https://hallym-festival.com", "http://github-action-s3-hallym-festival-develop.s3-website.ap-northeast-2.amazonaws.com", "http://github-action-s3-list-app.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:8080", "http://localhost:5175", "http://localhost:5173", "http://localhost:8081", "http://localhost:3000", "http://3.39.62.170", "http://13.209.218.51","https://kim-sun-woo.com/") // 허용할 출처 추가
                 .allowedMethods("GET", "POST", "DELETE", "PUT") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 //.allowedOriginPatterns("*") //추가한 부분
