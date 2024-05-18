bb933331886c5a3130f878303f8f69ee79e34db1
선우 데스크탑 <sam11222@naver.com>
2024-05-18 12:44:28 +0900

    Fix : 관리자 로그인 cors 수정(main)

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/WebConfig.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/WebConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("")
+        registry.addMapping("/")
                 .allowedOrigins("https://hallym-festival-admin.com")
                 .allowedMethods("GET", "POST", "DELETE", "PUT") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
