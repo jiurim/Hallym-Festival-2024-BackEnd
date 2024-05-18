831eea799e5dd7ee0f31018dc0bb9d91ddfd7a69
선우 데스크탑 <sam11222@naver.com>
2024-05-18 12:48:05 +0900

    Fix : 관리자 로그인 cors 수정(main)

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/SecurityConfig.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/SecurityConfig.java
@@ -52,80 +52,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests()
-                // HttpServletRequest를 사용하는 요청들에 대한 접근 제한 설정.
+                //HttpServletRequest를 사용하는 요청들에 대한 접근 제한 설정.
                 //.antMatchers("/api/admin/community", "/api/admin/notice", "/api/admin/find").hasRole("ADMIN")
-                // 위 주소는 관리자만 접근
-                .antMatchers("/admin/**").authenticated()
-                // /admin으로 시작하는 경로는 인증이 필요함
-                .anyRequest().permitAll()
-                // 나머지 모든 경로는 인증 없이 접근 가능
+                //위 주소는 관리자만 접근
+                .antMatchers("/**").permitAll()
+                .antMatchers("/api/admin/login").permitAll()
+                .antMatchers("api/admin/sign_up").permitAll()
+                .antMatchers("http://13.209.218.51/api/env").permitAll()
+                .antMatchers("http://13.209.218.51/api/hc").permitAll()
+                .antMatchers("http://3.39.62.170/api/env").permitAll()
+                .antMatchers("http://13.209.218.51/api/hc").permitAll()
+                .antMatchers("http://43.202.170.80:8080/api/env").permitAll()
+                .antMatchers("http://43.202.170.80:8080/api/hc").permitAll()
+                .antMatchers("http://43.202.170.80:8081/api/env").permitAll()
+                .antMatchers("http://43.202.170.80:8081/api/hc").permitAll()
+                .antMatchers("http://43.202.160.134:8080/api/env").permitAll()
+                .antMatchers("http://43.202.160.134:8080/api/hc").permitAll()
+                .antMatchers("http://43.202.160.134:8081/api/env").permitAll()
+                .antMatchers("http://43.202.160.134:8081/api/hc").permitAll()
+                .antMatchers("http://3.39.62.170").permitAll()
+                .antMatchers("http://13.209.218.51").permitAll()
+                //위 api는 인증 없이 접근 허용
+
                 .and()
-                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .csrf()
+                .ignoringAntMatchers("/**")
+                .ignoringAntMatchers("/api/admin/login")
+                .ignoringAntMatchers("/api/admin/sign_up");
+                //csrf 무시
     }
-
-
 }
-
-
-
-
-
-//@Configuration
-//@EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
-//@RequiredArgsConstructor
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    private final JwtTokenProvider jwtTokenProvider;
-//    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-//    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .httpBasic().disable()
-//                .csrf().disable()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//
-//                .and()
-//                .exceptionHandling()
-//                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//                .accessDeniedHandler(jwtAccessDeniedHandler)
-//
-//                .and()
-//                .authorizeRequests()
-//                //HttpServletRequest를 사용하는 요청들에 대한 접근 제한 설정.
-//                //.antMatchers("/api/admin/community", "/api/admin/notice", "/api/admin/find").hasRole("ADMIN")
-//                //위 주소는 관리자만 접근
-//                .antMatchers("/**").permitAll()
-//                .antMatchers("/api/admin/login").permitAll()
-//                .antMatchers("api/admin/sign_up").permitAll()
-//                .antMatchers("http://13.209.218.51/api/env").permitAll()
-//                .antMatchers("http://13.209.218.51/api/hc").permitAll()
-//                .antMatchers("http://3.39.62.170/api/env").permitAll()
-//                .antMatchers("http://13.209.218.51/api/hc").permitAll()
-//                .antMatchers("http://43.202.170.80:8080/api/env").permitAll()
-//                .antMatchers("http://43.202.170.80:8080/api/hc").permitAll()
-//                .antMatchers("http://43.202.170.80:8081/api/env").permitAll()
-//                .antMatchers("http://43.202.170.80:8081/api/hc").permitAll()
-//                .antMatchers("http://43.202.160.134:8080/api/env").permitAll()
-//                .antMatchers("http://43.202.160.134:8080/api/hc").permitAll()
-//                .antMatchers("http://43.202.160.134:8081/api/env").permitAll()
-//                .antMatchers("http://43.202.160.134:8081/api/hc").permitAll()
-//                .antMatchers("http://3.39.62.170").permitAll()
-//                .antMatchers("http://13.209.218.51").permitAll()
-//                //위 api는 인증 없이 접근 허용
-//
-//                .and()
-//                .csrf()
-//                .ignoringAntMatchers("/**")
-//                .ignoringAntMatchers("/api/admin/login")
-//                .ignoringAntMatchers("/api/admin/sign_up");
-//                //csrf 무시
-//    }
-//}
