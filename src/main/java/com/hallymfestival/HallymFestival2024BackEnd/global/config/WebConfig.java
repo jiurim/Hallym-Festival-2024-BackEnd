4bf23641b799e21fab036121c175b373ac8b65da
선우 데스크탑 <sam11222@naver.com>
2024-05-18 11:05:03 +0900

    Fix : 관리자 로그인 cors 수정(main)

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/WebConfig.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 //.allowedOriginPatterns("*") //추가한 부분
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱2
-        registry.addMapping("/login")
+        registry.addMapping("/")
                 .allowedOrigins("https://hallym-festival-admin.com")
                 .allowedMethods("GET", "POST", "DELETE", "PUT")
                 .allowCredentials(true)
