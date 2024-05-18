bae9d2feb5684092e03343eeeefe02e1c38b9268
선우 데스크탑 <sam11222@naver.com>
2024-05-18 14:24:33 +0900

    Fix : 관리자 로그인 cors 수정(main)

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/SecurityConfig.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/SecurityConfig.java
@@ -44,8 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeRequests()
-                .antMatchers("/*/login").permitAll()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/api/hc","/api/env","https://kim-sun-woo.com/api/admin/login").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/api/admin/**").permitAll()
                 .anyRequest().hasAnyRole("ADMIN")
                 .and().addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
