174ecd0f0a782c00b0a2810092b854efdb32570d
선우 데스크탑 <sam11222@naver.com>
2024-05-18 13:00:38 +0900

    Fix : 관리자 로그인 cors 수정(main)

--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/SecurityConfig.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/SecurityConfig.java
@@ -42,43 +42,74 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .httpBasic().disable()
                 .csrf().disable()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                .accessDeniedHandler(jwtAccessDeniedHandler)
-
-                .and()
-                .authorizeRequests()
-                //HttpServletRequest를 사용하는 요청들에 대한 접근 제한 설정.
-                //.antMatchers("/api/admin/community", "/api/admin/notice", "/api/admin/find").hasRole("ADMIN")
-                //위 주소는 관리자만 접근
-                .antMatchers("/**").permitAll()
-                .antMatchers("/api/admin/login").permitAll()
-                .antMatchers("api/admin/sign_up").permitAll()
-                .antMatchers("http://13.209.218.51/api/env").permitAll()
-                .antMatchers("http://13.209.218.51/api/hc").permitAll()
-                .antMatchers("http://3.39.62.170/api/env").permitAll()
-                .antMatchers("http://13.209.218.51/api/hc").permitAll()
-                .antMatchers("http://43.202.170.80:8080/api/env").permitAll()
-                .antMatchers("http://43.202.170.80:8080/api/hc").permitAll()
-                .antMatchers("http://43.202.170.80:8081/api/env").permitAll()
-                .antMatchers("http://43.202.170.80:8081/api/hc").permitAll()
-                .antMatchers("http://43.202.160.134:8080/api/env").permitAll()
-                .antMatchers("http://43.202.160.134:8080/api/hc").permitAll()
-                .antMatchers("http://43.202.160.134:8081/api/env").permitAll()
-                .antMatchers("http://43.202.160.134:8081/api/hc").permitAll()
-                .antMatchers("http://3.39.62.170").permitAll()
-                .antMatchers("http://13.209.218.51").permitAll()
-                //위 api는 인증 없이 접근 허용
-
-                .and()
-                .csrf()
-                .ignoringAntMatchers("/**")
-                .ignoringAntMatchers("/api/admin/login")
-                .ignoringAntMatchers("/api/admin/sign_up");
-                //csrf 무시
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeRequests()
+                .antMatchers("/*/login").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/api/admin/**").permitAll()
+                .anyRequest().hasAnyRole("ADMIN")
+                .and().addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
     }
+
 }
+
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@RequiredArgsConstructor
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    private final JwtTokenProvider jwtTokenProvider;
+//    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+//    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .httpBasic().disable()
+//                .csrf().disable()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//
+//                .and()
+//                .exceptionHandling()
+//                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+//                .accessDeniedHandler(jwtAccessDeniedHandler)
+//
+//                .and()
+//                .authorizeRequests()
+//                //HttpServletRequest를 사용하는 요청들에 대한 접근 제한 설정.
+//                //.antMatchers("/api/admin/community", "/api/admin/notice", "/api/admin/find").hasRole("ADMIN")
+//                //위 주소는 관리자만 접근
+//                .antMatchers("/**").permitAll()
+//                .antMatchers("/api/admin/login").permitAll()
+//                .antMatchers("api/admin/sign_up").permitAll()
+//                .antMatchers("http://13.209.218.51/api/env").permitAll()
+//                .antMatchers("http://13.209.218.51/api/hc").permitAll()
+//                .antMatchers("http://3.39.62.170/api/env").permitAll()
+//                .antMatchers("http://13.209.218.51/api/hc").permitAll()
+//                .antMatchers("http://43.202.170.80:8080/api/env").permitAll()
+//                .antMatchers("http://43.202.170.80:8080/api/hc").permitAll()
+//                .antMatchers("http://43.202.170.80:8081/api/env").permitAll()
+//                .antMatchers("http://43.202.170.80:8081/api/hc").permitAll()
+//                .antMatchers("http://43.202.160.134:8080/api/env").permitAll()
+//                .antMatchers("http://43.202.160.134:8080/api/hc").permitAll()
+//                .antMatchers("http://43.202.160.134:8081/api/env").permitAll()
+//                .antMatchers("http://43.202.160.134:8081/api/hc").permitAll()
+//                .antMatchers("http://3.39.62.170").permitAll()
+//                .antMatchers("http://13.209.218.51").permitAll()
+//                //위 api는 인증 없이 접근 허용
+//
+//                .and()
+//                .csrf()
+//                .ignoringAntMatchers("/**")
+//                .ignoringAntMatchers("/api/admin/login")
+//                .ignoringAntMatchers("/api/admin/sign_up");
+//        //csrf 무시
+//    }
+//}
--- src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/WebConfig.java
+++ src/main/java/com/hallymfestival/HallymFestival2024BackEnd/global/config/WebConfig.java
@@ -5,17 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+
 // 스프링 서버 전역적으로 CORS 설정
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer  {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/")
-                .allowedOrigins("https://hallym-festival-admin.com")
-                .allowedMethods("GET", "POST", "DELETE", "PUT") // 허용할 HTTP method
-                .allowCredentials(true) // 쿠키 인증 요청 허용
-                //.allowedOriginPatterns("*") //추가한 부분
-                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱2
         registry.addMapping("/**")
                 .allowedOrigins("http://hallym-festival.com", "https://hallym-festival.com", "http://github-action-s3-hallym-festival-develop.s3-website.ap-northeast-2.amazonaws.com", "http://github-action-s3-list-app.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:8080", "http://localhost:5175", "http://localhost:5173", "http://localhost:8081", "http://localhost:3000", "http://3.39.62.170", "http://13.209.218.51") // 허용할 출처 추가
                 .allowedMethods("GET", "POST", "DELETE", "PUT") // 허용할 HTTP method
@@ -23,5 +18,20 @@ public class WebConfig implements WebMvcConfigurer {
                 //.allowedOriginPatterns("*") //추가한 부분
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱2
     }
+
+}
+
+//// 스프링 서버 전역적으로 CORS 설정
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://hallym-festival.com", "https://hallym-festival.com", "http://github-action-s3-hallym-festival-develop.s3-website.ap-northeast-2.amazonaws.com", "http://github-action-s3-list-app.s3-website.ap-northeast-2.amazonaws.com", "http://localhost:8080", "http://localhost:5175", "http://localhost:5173", "http://localhost:8081", "http://localhost:3000", "http://3.39.62.170", "http://13.209.218.51") // 허용할 출처 추가
+//                .allowedMethods("GET", "POST", "DELETE", "PUT") // 허용할 HTTP method
+//                .allowCredentials(true) // 쿠키 인증 요청 허용
+//                //.allowedOriginPatterns("*") //추가한 부분
+//                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱2
+//    }
 //
-    }
+//    }
